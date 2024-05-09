695378a32d565e00e6264c91243bb7a3957f7c48
Zawaken <zawaken@outlook.com>
2024-05-09 21:21:58 +0200

    Thu May  9 09:21:58 PM CEST 2024

--- config/qmk.nix
+++ config/qmk.nix
@@ -1,27 +1,36 @@
 { lib, ...}:
-let ortho4x12 = [
-  "x x x x x x x x x x x x"
-  "x x x x x x x x x x x x"
-  "x x x x x x x x x x x x"
-  "x x x x x x x x x x x x"
-];
-ortho4x12LayoutString = lib.strings.concatMapStringsSep ",\n" (s: "'${s}'") ortho4x12;
+let
+  layoutToString = layout: lib.strings.concatMapStringsSep ",\n" (s: "'${s}'") layout;
 
-ortho5x12 = [
-  "x x x x x x x x x x x x"
-  "x x x x x x x x x x x x"
-  "x x x x x x x x x x x x"
-  "x x x x x x x x x x x x"
-  "x x x x x x x x x x x x"
-];
-ortho5x12LayoutString = lib.strings.concatMapStringsSep ",\n" (s: "'${s}'") ortho5x12;
+  generateLayout = rows: cols:
+    let
+      row = lib.concatStringsSep " " (lib.replicate cols "x");
+    in
+      lib.replicate rows row;
+
+  createKeyboardCallback = args:
+    let
+      name = builtins.elemAt args 0;
+      layout = builtins.elemAt args 1;
+      variant = if builtins.length args > 2 then builtins.elemAt args 2 else "qmk";
+    in  {
+    __raw = ''
+      function() require('qmk').setup({
+        name = '${name}',
+        variant = '${variant}',
+        layout = {
+          ${layoutToString layout}
+        }
+      }) end
+    '';
+  };
 
 in {
   plugins.qmk = {
     enable = true;
     settings = {
       name = "LAYOUT_preonic_grid";
-      layout = ortho5x12;
+      layout = generateLayout 5 12;
     };
   };
 
@@ -31,42 +40,31 @@ in {
       group = "Qmk";
       event = [ "BufEnter" ];
       pattern = [ "*preonic/keymap.c" ];
-      callback = { __raw = "function() require('qmk').setup({
-                    name = 'LAYOUT_preonic_grid',
-                    layout = {
-                      ${ortho5x12LayoutString}
-                    }}) end"; };
+      callback = createKeyboardCallback "LAYOUT_preonic_grid" (generateLayout 5 12);
     }
     { # planck keymap
       group = "Qmk";
       event = [ "BufEnter" ];
       pattern = [ "*planck/keymap.c" ];
-      callback = { __raw = "function() require('qmk').setup({
-                    name = 'LAYOUT_planck_grid',
-                    comment_preview = { position = 'inside' },
-                    layout = {
-                      ${ortho4x12LayoutString}
-                    }}) end"; };
+      callback = createKeyboardCallback "LAYOUT_planck_grid" (generateLayout 4 12);
     }
     { # framework/ortho_5x12 keymap
       group = "Qmk";
       event = [ "BufEnter" ];
       pattern = [ "*framework/keymap.c" ];
-      callback = { __raw = "function() require('qmk').setup({
-                    name = 'LAYOUT_ortho_5x12',
-                    layout = {
-                      ${ortho5x12LayoutString}
-                    }}) end"; };
+      callback = createKeyboardCallback "LAYOUT_ortho_5x12" (generateLayout 5 12);
     }
     { # Air40/ortho_4x12 layout
       group = "Qmk";
       event = [ "BufEnter" ];
       pattern = [ "*air40/keymap.c" ];
-      callback = { __raw = "function() require('qmk').setup({
-                    name = 'LAYOUT_ortho_4x12',
-                    layout = {
-                      ${ortho4x12LayoutString}
-                    }}) end"; };
+      callback = createKeyboardCallback "LAYOUT_ortho_4x12" (generateLayout 4 12);
+    }
+    { # Blank space layout, currently doesn't work
+      group = "Qmk";
+      event = [ "BufEnter" ];
+      pattern = [ "*.keymap" ];
+      callback = createKeyboardCallback "meh" (generateLayout 4 12) "zmk";
     }
     { # Boardsource Lulu layout
       group = "Qmk";
@@ -80,17 +78,6 @@ in {
                       'x x x x x x _ _ _ x x x x x x',
                       'x x x x x x x _ x x x x x x x',
                       '_ _ _ x x x x _ x x x x _ _ _'
-                    }}) end"; };
-    }
-    { # Blank space layout, currently doesn't work
-      group = "Qmk";
-      event = [ "BufEnter" ];
-      pattern = [ "*.keymap" ];
-      callback = { __raw = "function() require('qmk').setup({
-                    name = 'meh',
-                    variant = 'zmk',
-                    layout = {
-                      ${ortho4x12LayoutString}
                     }}) end"; };
     }
   ];
