c044bdc6f8777e3768a4b2a91defd4dcef3559a4
Zawaken <zawaken@outlook.com>
2024-05-28 20:55:22 +0200

    make neorg actually work

--- flake.nix
+++ flake.nix
@@ -5,15 +5,18 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
     nixvim.url = "github:nix-community/nixvim";
+    neorg.url = "github:nvim-neorg/nixpkgs-neorg-overlay";
   };
 
-  outputs = inputs@{ nixpkgs, nixvim, utils, ... }:
+  outputs = inputs@{ nixpkgs, nixvim, utils, neorg, ... }:
     utils.lib.eachDefaultSystem (system:
       let
         lib = import ./lib.nix {
           inherit inputs system;
         };
-        pkgs = import nixpkgs { inherit system; };
+        # Needed to make neorg work after upgrade to 8.0.0 https://github.com/nix-community/nixvim/issues/1395
+        overlays = [ neorg.overlays.default ];
+        pkgs = import nixpkgs { inherit system overlays; };
         nixvimLib = nixvim.lib.${system};
         nixvim' = nixvim.legacyPackages.${system};
         nvim = nixvim'.makeNixvimWithModule {
